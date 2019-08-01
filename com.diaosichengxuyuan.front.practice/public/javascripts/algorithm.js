e2eeec10530fc7973a77d5ae26f2eff0084b2cb0
“liuhaipeng” <>
2019-08-01 22:08:34 +0800

    增加4个leecode算法题

--- com.diaosichengxuyuan.front.practice/public/javascripts/algorithm.js
+++ com.diaosichengxuyuan.front.practice/public/javascripts/algorithm.js
@@ -87,12 +87,12 @@ const lengthOfLongestSubstring = function (s) {
     return maxLength;
 };
 
-//请你来实现一个 atoi 函数，使其能将字符串转换成整数。
+//请你来实现一个atoi函数，使其能将字符串转换成整数。
 //首先，该函数会根据需要丢弃无用的开头空格字符，直到寻找到第一个非空格的字符为止。
 //当我们寻找到的第一个非空字符为正或者负号时，则将该符号与之后面尽可能多的连续数字组合起来，作为该整数的正负号；假如第一个非空字符是数字，则直接将其与之后连续的数字字符组合起来，形成整数。
 //该字符串除了有效的整数部分之后也可能会存在多余的字符，这些字符可以被忽略，它们对于函数不应该造成影响。
 //注意：假如该字符串中的第一个非空格字符不是一个有效整数字符、字符串为空或字符串仅包含空白字符时，则你的函数不需要进行转换。在任何情况下，若函数不能进行有效的转换时，请返回0。
-//说明：假设我们的环境只能存储32位大小的有符号整数，那么其数值范围为[−231,231−1]。如果数值超过这个范围，请返回INT_MAX(231−1)或INT_MIN(−231)。
+//说明：假设我们的环境只能存储32位大小的有符号整数，那么其数值范围为[−2^31,2^31−1]。如果数值超过这个范围，请返回INT_MAX(2^31−1)或INT_MIN(−2^31)。
 var atoi = function (str) {
     if (StringUtil.isEmpty(str)) {
         return 0;
@@ -149,6 +149,7 @@ var atoi = function (str) {
 
 //给定一个按照升序排列的整数数组nums，和一个目标值target。找出给定目标值在数组中的开始位置和结束位置。
 //你的算法时间复杂度必须是O(log n)级别。如果数组中不存在目标值，返回[-1,-1]。
+//二分查找
 const searchRange = function (nums, target) {
     if (nums == null || nums.length == 0 || target == null) {
         return [-1, -1];
@@ -197,6 +198,131 @@ const searchRange = function (nums, target) {
     return [leftIndex, rightIndex];
 };
 
+//给定一个包含红色、白色和蓝色，一共n个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+//此题中，我们使用整数0、1和2分别表示红色、白色和蓝色。不能使用代码库中的排序函数来解决这道题。
+//三指针算法
+const sortColors = function (nums) {
+    let leftIndex = 0;
+    let rightIndex = nums.length - 1;
+    let currentIndex = 0;
+
+    while (nums[leftIndex] == 0) {
+        leftIndex++;
+    }
+    if (leftIndex >= nums.length - 1) {
+        return nums;
+    }
+
+    while (nums[rightIndex] == 2) {
+        rightIndex--;
+    }
+    if (rightIndex <= 0) {
+        return nums;
+    }
+
+    if (leftIndex >= rightIndex) {
+        return nums;
+    }
+
+    currentIndex = leftIndex;
+
+    while (currentIndex <= rightIndex && leftIndex < rightIndex) {
+        if (nums[currentIndex] == 2) {
+            const temp = nums[currentIndex];
+            nums[currentIndex] = nums[rightIndex];
+            nums[rightIndex] = temp;
+            rightIndex--;
+        } else if (nums[currentIndex] == 0) {
+            const temp = nums[currentIndex];
+            nums[currentIndex] = nums[leftIndex];
+            nums[leftIndex] = temp;
+            leftIndex++;
+            if (leftIndex > currentIndex) {
+                currentIndex++;
+            }
+        } else {
+            currentIndex++;
+        }
+    }
+
+    return nums;
+};
+
+//给定一个二叉树，返回它的中序遍历。
+//莫里斯遍历
+const inorderTraversal = function (root) {
+    const result = [];
+    let currentNode = root;
+    while (currentNode != null) {
+        if (currentNode.left == null) {
+            result.push(currentNode.val);
+            currentNode = currentNode.right;
+        } else {
+            const nextCurrentNode = currentNode.left;
+            currentNode.left = null;
+            const mostRightNode = findMostRightNode(nextCurrentNode);
+            mostRightNode.right = currentNode;
+            currentNode = nextCurrentNode;
+        }
+    }
+
+    return result;
+};
+
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+function findMostRightNode(node) {
+    while (node.right != null) {
+        node = node.right;
+    }
+
+    return node;
+}
+
+//给出n代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+var generateParenthesis = function (n) {
+    return Array.from(generateParenthesisInner(n));
+};
+
+function generateParenthesisInner(n) {
+    if (n == 1) {
+        const set = new Set();
+        set.add("()");
+        return set;
+    }
+
+    const set = new Set();
+    const parenthesisSet = generateParenthesisInner(n - 1);
+    for (let parenthesis of parenthesisSet) {
+        for (let index = 0; index < parenthesis.length; index++) {
+            set.add(parenthesis.substring(0, index) + "()" + parenthesis.substring(index));
+        }
+        set.add(parenthesis + "()");
+    }
+
+    return set;
+};
+
+//给定一个包含非负整数的MxN网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+//说明：每次只能向下或者向右移动一步。
+//动态规划，最短路径
+var minPathSum = function (grid) {
+    const temp = [0];
+    for (let i in grid) {
+        const row = grid[i];
+        for (let j in row) {
+            const currentElem = row[j];
+            const leftElem = temp[j - 1];
+            const aboveElem = temp[j];
+            temp[j] = Math.min(leftElem == null ? Number.MAX_VALUE : leftElem, aboveElem == null ? Number.MAX_VALUE : aboveElem) + currentElem;
+        }
+    }
+
+    return temp[temp.length - 1];
+};
 
 const test = function () {
 }
